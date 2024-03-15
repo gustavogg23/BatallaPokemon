cf6e6d1d3187ee4eb51d134233ef0313d4998426
gustavogg23 <ggutierrez.7080@unimar.edu.ve>
2024-03-15 00:38:11 -0400

    Se creó la clase Nodo

---
+++ BatallaPokemon/Nodo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatallaPokemon
+{
+    public class Nodo
+    {
+        //Atributos
+        private Pokemon valor;
+        private Nodo siguiente;
+
+        //Método constructor
+        public Nodo(Pokemon valor)
+        {
+            this.valor = valor;
+            this.siguiente = null;
+        }
+
+        //Métodos Getters y Setters
+        public Pokemon getValor()
+        {
+            return valor;
+        }
+
+        public void setValor(Pokemon valor)
+        {
+            this.valor = valor;
+        }
+
+        public Nodo getSiguiente()
+        {
+            return siguiente;
+        }
+
+        public void setSiguiente(Nodo siguiente)
+        {
+            this.siguiente = siguiente;
+        }
+    }
+}
