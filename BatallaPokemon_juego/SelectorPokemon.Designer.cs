7edbf2bb96de9a3588ac775e5640dff847336c52
gustavogg23 <ggutierrez.7080@unimar.edu.ve>
2024-03-28 03:06:08 -0400

    Cambios

--- BatallaPokemon_juego/SelectorPokemon.Designer.cs
+++ BatallaPokemon_juego/SelectorPokemon.Designer.cs
@@ -31,15 +31,19 @@
             this.cerrar = new System.Windows.Forms.PictureBox();
             this.minimizar = new System.Windows.Forms.PictureBox();
             this.aceptar = new System.Windows.Forms.Button();
+            this.lblPokemones = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelPokemones = new System.Windows.Forms.FlowLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.cerrar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.minimizar)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // cerrar
             // 
             this.cerrar.BackColor = System.Drawing.Color.Transparent;
             this.cerrar.Image = global::BatallaPokemon_juego.Properties.Resources.salir;
-            this.cerrar.Location = new System.Drawing.Point(758, 12);
+            this.cerrar.Location = new System.Drawing.Point(80, 3);
             this.cerrar.Name = "cerrar";
             this.cerrar.Size = new System.Drawing.Size(30, 30);
             this.cerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -51,7 +55,7 @@
             // 
             this.minimizar.BackColor = System.Drawing.Color.Transparent;
             this.minimizar.Image = global::BatallaPokemon_juego.Properties.Resources.Minimizar;
-            this.minimizar.Location = new System.Drawing.Point(716, 12);
+            this.minimizar.Location = new System.Drawing.Point(45, 3);
             this.minimizar.Name = "minimizar";
             this.minimizar.Size = new System.Drawing.Size(30, 30);
             this.minimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -62,7 +66,7 @@
             // aceptar
             // 
             this.aceptar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.aceptar.Location = new System.Drawing.Point(648, 401);
+            this.aceptar.Location = new System.Drawing.Point(840, 532);
             this.aceptar.Name = "aceptar";
             this.aceptar.Size = new System.Drawing.Size(140, 37);
             this.aceptar.TabIndex = 2;
@@ -70,22 +74,61 @@
             this.aceptar.UseVisualStyleBackColor = true;
             this.aceptar.Click += new System.EventHandler(this.aceptar_Click);
             // 
+            // lblPokemones
+            // 
+            this.lblPokemones.BackColor = System.Drawing.Color.Transparent;
+            this.lblPokemones.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblPokemones.Font = new System.Drawing.Font("Agency FB", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPokemones.Location = new System.Drawing.Point(0, 0);
+            this.lblPokemones.Name = "lblPokemones";
+            this.lblPokemones.Size = new System.Drawing.Size(992, 60);
+            this.lblPokemones.TabIndex = 3;
+            this.lblPokemones.Text = "POKEMONES";
+            this.lblPokemones.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.minimizar);
+            this.panel1.Controls.Add(this.cerrar);
+            this.panel1.Location = new System.Drawing.Point(870, 9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(110, 51);
+            this.panel1.TabIndex = 4;
+            // 
+            // panelPokemones
+            // 
+            this.panelPokemones.AutoScroll = true;
+            this.panelPokemones.BackColor = System.Drawing.Color.Transparent;
+            this.panelPokemones.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelPokemones.Location = new System.Drawing.Point(0, 60);
+            this.panelPokemones.Name = "panelPokemones";
+            this.panelPokemones.Size = new System.Drawing.Size(824, 521);
+            this.panelPokemones.TabIndex = 5;
+            this.panelPokemones.Paint += new System.Windows.Forms.PaintEventHandler(this.panelPokemones_Paint);
+            // 
             // SelectorPokemon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lavender;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::BatallaPokemon_juego.Properties.Resources.ElegirPokemones;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(992, 581);
+            this.Controls.Add(this.panelPokemones);
             this.Controls.Add(this.aceptar);
-            this.Controls.Add(this.minimizar);
-            this.Controls.Add(this.cerrar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblPokemones);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.Name = "SelectorPokemon";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SelectorPokemon";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.SelectorPokemon_Load);
             ((System.ComponentModel.ISupportInitialize)(this.cerrar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.minimizar)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -95,5 +138,8 @@
         private System.Windows.Forms.PictureBox cerrar;
         private System.Windows.Forms.PictureBox minimizar;
         private System.Windows.Forms.Button aceptar;
+        private System.Windows.Forms.Label lblPokemones;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel panelPokemones;
     }
 }
--- BatallaPokemon_juego/SelectorPokemon.cs
+++ BatallaPokemon_juego/SelectorPokemon.cs
@@ -34,5 +34,38 @@ namespace BatallaPokemon_juego
             BC_BATALLA formBC_BATALLA = new BC_BATALLA();
             formBC_BATALLA.ShowDialog();
         }
+
+        private void panelPokemones_Paint(object sender, PaintEventArgs e) // NOTA: Este método no se utiliza NO BORRAR
+        {
+      
+        }
+
+        private void CrearBotonesPokemones()
+        {
+            panelPokemones.Controls.Clear();
+
+            for (int i = 1; i <= 151; i++)
+            {
+                Pokemon pokemon = BarraCarga.DatosListas.listaPokemones.buscar(i); //busca el pokemon en la lista de pokemones según su número
+
+                Button btnPokemon = new Button(); // Se crea un nuevo botón
+                btnPokemon.Size = new Size(130, 122); // Se establece el tamaño del botón
+                btnPokemon.FlatStyle = FlatStyle.Flat; // Se establece el estilo del botón
+                btnPokemon.BackColor = Color.Transparent; // Se establece el color de fondo del botón
+                btnPokemon.BackgroundImage = (Bitmap)Properties.Resources.ResourceManager.GetObject(pokemon.getImagen()); // Se establece la imagen de fondo del botón
+                btnPokemon.BackgroundImageLayout = ImageLayout.Stretch; // Se establece el diseño de la imagen de fondo del botón
+                btnPokemon.FlatAppearance.BorderSize = 0; // Se establece el tamaño del borde del botón
+                btnPokemon.FlatAppearance.MouseDownBackColor = Color.Transparent; // Se establece el color de fondo del botón cuando se presiona
+                btnPokemon.FlatAppearance.MouseOverBackColor = Color.Transparent; // Se establece el color de fondo del botón cuando el mouse pasa por encima
+                btnPokemon.Cursor = Cursors.Hand; // Se establece el cursor del botón
+                btnPokemon.Margin = new Padding(10); // Se establece el margen del botón
+                panelPokemones.Controls.Add(btnPokemon); // Se agrega el botón al panel de pokemones
+            }
+        }
+
+        private void SelectorPokemon_Load(object sender, EventArgs e)
+        {
+            CrearBotonesPokemones();
+        }
     }
 }
