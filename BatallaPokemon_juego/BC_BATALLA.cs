8d7fc85fa7138c867b0b3e4184b5d243f74de7c6
gustavogg23 <ggutierrez.7080@unimar.edu.ve>
2024-03-29 01:46:04 -0400

    Implementando método para crear cada item y crear una pila para cada item y llenar cada pila para luego agregar cada pila a la lista de items que tiene cada jugador.

--- BatallaPokemon_juego/BC_BATALLA.cs
+++ BatallaPokemon_juego/BC_BATALLA.cs
@@ -32,6 +32,9 @@ namespace BatallaPokemon_juego
         {
             player.Play(); //para iniciar la reproduccion
 
+            // Se llama al método para agregar los items a la lista de items del entrenador
+            AgregarItems();
+
             Timer timer = new Timer();
             timer.Interval = 100; //Intervalo de actualizacion de la barra
             timer.Tick += (s, e) =>
@@ -70,6 +73,76 @@ namespace BatallaPokemon_juego
         private void minimizar_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+
+        // Método para agregar los items a la lista de items del entrenador para poder usar en batalla
+        private void AgregarItems()
+        {
+            // Se crea cada objeto Item con su nombre y cantidad
+            Item pocion = new Item("Poción", 3);
+            Item superPocion = new Item("Superpoción", 2);
+            Item hiperPocion = new Item("Hiperpoción", 1);
+            Item antiparalisis = new Item("Antiparálisis", 3);
+            Item antidoto = new Item("Antídoto", 2);
+            Item despertar = new Item("Despertar", 3);
+            Item curaTotal = new Item("Cura Total", 1);
+            Item maxPocion = new Item("Max Poción", 1);
+            Item precisionX = new Item("Precisión X", 6);
+
+            // Se crea una pila para cada item
+            PilaItems pilaPocion = new PilaItems();
+            PilaItems pilaSuperPocion = new PilaItems();
+            PilaItems pilaHiperPocion = new PilaItems();
+            PilaItems pilaAntiparalisis = new PilaItems();
+            PilaItems pilaAntidoto = new PilaItems();
+            PilaItems pilaDespertar = new PilaItems();
+            PilaItems pilaCuraTotal = new PilaItems();
+            PilaItems pilaMaxPocion = new PilaItems();
+            PilaItems pilaPrecisionX = new PilaItems();
+
+            // Se agrega cada item a su respectiva pila
+            for (int i = 0; i < 3; i++)
+            {
+                pilaPocion.push(pocion);
+                pilaAntiparalisis.push(antiparalisis);
+                pilaDespertar.push(despertar);
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                pilaSuperPocion.push(superPocion);
+                pilaAntidoto.push(antidoto);
+            }
+
+            pilaHiperPocion.push(hiperPocion);
+            pilaCuraTotal.push(curaTotal);
+            pilaMaxPocion.push(maxPocion);
+
+            for (int i = 0; i < 6; i++)
+            {
+                pilaPrecisionX.push(precisionX);
+            }
+
+            // Se agregan las pilas de items a la lista de items de los jugadores
+            LOGIN.DatosJugadores.player1.agregarItem(pilaPocion);
+            LOGIN.DatosJugadores.player1.agregarItem(pilaSuperPocion);
+            LOGIN.DatosJugadores.player1.agregarItem(pilaHiperPocion);
+            LOGIN.DatosJugadores.player1.agregarItem(pilaAntiparalisis);
+            LOGIN.DatosJugadores.player1.agregarItem(pilaAntidoto);
+            LOGIN.DatosJugadores.player1.agregarItem(pilaDespertar);
+            LOGIN.DatosJugadores.player1.agregarItem(pilaCuraTotal);
+            LOGIN.DatosJugadores.player1.agregarItem(pilaMaxPocion);
+            LOGIN.DatosJugadores.player1.agregarItem(pilaPrecisionX);
+
+            LOGIN.DatosJugadores.player2.agregarItem(pilaPocion);
+            LOGIN.DatosJugadores.player2.agregarItem(pilaSuperPocion);
+            LOGIN.DatosJugadores.player2.agregarItem(pilaHiperPocion);
+            LOGIN.DatosJugadores.player2.agregarItem(pilaAntiparalisis);
+            LOGIN.DatosJugadores.player2.agregarItem(pilaAntidoto);
+            LOGIN.DatosJugadores.player2.agregarItem(pilaDespertar);
+            LOGIN.DatosJugadores.player2.agregarItem(pilaCuraTotal);
+            LOGIN.DatosJugadores.player2.agregarItem(pilaMaxPocion);
+            LOGIN.DatosJugadores.player2.agregarItem(pilaPrecisionX);
         }
     }
 }
